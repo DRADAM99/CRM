dc187d66be38e622cade6529032cecf2ae15ff82
Adam Winter <adawint@gmail.com>
2025-06-30 00:59:01 +0300

    feat: update CardTitle sizes, set Rubik font, and configure Tailwind for Rubik; add @fontsource/rubik dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -94,6 +94,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ['Rubik', 'Arial', 'Helvetica', 'sans-serif'],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
